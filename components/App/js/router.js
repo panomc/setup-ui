9fcc46851a28172f143fdfcfa758941457b07a78
kahverengi <ahmet.duruer@yandex.com>
2019-03-04 16:05:08 +0100

    Added sample pages

--- components/App/js/router.js
+++ components/App/js/router.js
@@ -2,16 +2,32 @@ const router = new VueRouter({
     mode: 'history',
     routes: [
         {
-            path: '/',
+            path: '*',
+            name: 'Error404',
             component: function (resolve, reject) {
-                loadComponent('Step_1', '/components/Pages/Step_1/js/Step_1.js').then(resolve, reject);
+                loadComponent('Error404', '/components/Pages/Error404/js/Error404.js').then(resolve, reject);
             }
         },
         {
-            path: '/contact',
+            path: '/',
+            name: 'Main',
             component: function (resolve, reject) {
-                loadComponent('contact', 'contact.js').then(resolve, reject);
-            }
+                loadComponent('Main', '/components/Pages/Main/js/Main.js').then(resolve, reject);
+            },
+            children: [
+                {
+                    path: '',
+                    component: function (resolve, reject) {
+                        loadComponent('Step_1', '/components/Pages/Main/Steps/Step_1/js/Step_1.js').then(resolve, reject);
+                    }
+                },
+                {
+                    path: 'Step-2',
+                    component: function (resolve, reject) {
+                        loadComponent('Step_2', '/components/Pages/Main/Steps/Step_2/js/Step_2.js').then(resolve, reject);
+                    }
+                }
+            ]
         }
     ]
 });
--- components/Pages/Main/Steps/Step_1/js/Step_1.js
+++ components/Pages/Main/Steps/Step_1/js/Step_1.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Vue.component('Step_1', new Promise(function (resolve) {
-        axios.get('/components/Pages/Step_1/ui').then(function (response) {
+    axios.get('/components/Pages/Main/Steps/Step_1/ui').then(function (response) {
             resolve({
                 template: response.data
             });
