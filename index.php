f25f867d18febf34d5c9d6671418d0b95f9ec3f0
kahverengi <ahmet.duruer@yandex.com>
2019-03-05 17:56:06 +0100

    Removed title and changed tag

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="tr">
+<html>
 
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Pano Web Platformu</title>
+    <title>Pano</title>
 </head>
 
 <body>
