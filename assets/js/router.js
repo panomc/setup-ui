878b08611e72042caf5b0ebbf7d84c9e6e26b831
kahverengi <ahmet.duruer@yandex.com>
2019-05-23 20:58:22 +0200

    Reformatted codes for new coding style: PHP 4 spaces. CSS, SCSS, HTML, JS files 2 spaces.

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,57 +1,57 @@
 const router = new VueRouter({
-    mode: 'history',
-    routes: [
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'Main',
+      component: function (resolve, reject) {
+        loadComponent('Main', '/components/Pages/Main').then(resolve, reject);
+      },
+      children: [
         {
-            path: '/',
-            name: 'Main',
-            component: function (resolve, reject) {
-                loadComponent('Main', '/components/Pages/Main').then(resolve, reject);
-            },
-            children: [
-                {
-                    path: '',
-                    component: function (resolve, reject) {
-                        loadComponent('Step_Beginning', '/components/Pages/Main/Steps/Step_Beginning').then(resolve, reject);
-                    }
-                },
-                {
-                    path: 'Step-1',
-                    component: function (resolve, reject) {
-                        loadComponent('Step_1', '/components/Pages/Main/Steps/Step_1').then(resolve, reject);
-                    }
-                },
-                {
-                    path: 'Step-2',
-                    component: function (resolve, reject) {
-                        loadComponent('Step_2', '/components/Pages/Main/Steps/Step_2').then(resolve, reject);
-                    }
-                },
-                {
-                    path: 'Step-3',
-                    component: function (resolve, reject) {
-                        loadComponent('Step_3', '/components/Pages/Main/Steps/Step_3').then(resolve, reject);
-                    }
-                }
-            ]
+          path: '',
+          component: function (resolve, reject) {
+            loadComponent('Step_Beginning', '/components/Pages/Main/Steps/Step_Beginning').then(resolve, reject);
+          }
+        },
+        {
+          path: 'Step-1',
+          component: function (resolve, reject) {
+            loadComponent('Step_1', '/components/Pages/Main/Steps/Step_1').then(resolve, reject);
+          }
+        },
+        {
+          path: 'Step-2',
+          component: function (resolve, reject) {
+            loadComponent('Step_2', '/components/Pages/Main/Steps/Step_2').then(resolve, reject);
+          }
+        },
+        {
+          path: 'Step-3',
+          component: function (resolve, reject) {
+            loadComponent('Step_3', '/components/Pages/Main/Steps/Step_3').then(resolve, reject);
+          }
         }
-    ]
+      ]
+    }
+  ]
 });
 
 
 router.beforeEach((to, from, next) => {
-    const check = function () {
-        if (store.state.stepChecked !== false) {
-            store.state.routePageLoading = true
+  const check = function () {
+    if (store.state.stepChecked !== false) {
+      store.state.routePageLoading = true
 
-            next()
-        } else {
-            setTimeout(check, 100); // check again in a second
-        }
+      next()
+    } else {
+      setTimeout(check, 100); // check again in a second
     }
+  }
 
-    check()
+  check()
 })
 
 router.afterEach((to, from) => {
-    store.state.routePageLoading = false
+  store.state.routePageLoading = false
 })
--- components/Pages/Main/Steps/Step_Beginning/index.js
+++ components/Pages/Main/Steps/Step_Beginning/index.js
@@ -1,24 +1,24 @@
 'use strict';
 
 Vue.component('Step_Beginning', new Promise(function (resolve) {
-        axios.get('/components/Pages/Main/Steps/Step_Beginning/ui').then(function (response) {
-            resolve({
-                template: response.data,
-                data() {
-                    return {
-                        nextButtonLoading: false
-                    }
-                },
-                methods: {
-                    start() {
-                        this.nextButtonLoading = true
+    axios.get('/components/Pages/Main/Steps/Step_Beginning/ui').then(function (response) {
+      resolve({
+        template: response.data,
+        data() {
+          return {
+            nextButtonLoading: false
+          }
+        },
+        methods: {
+          start() {
+            this.nextButtonLoading = true
 
-                        this.$store.dispatch("nextStep", {
-                            step: 0
-                        })
-                    }
-                }
-            });
-        });
-    })
+            this.$store.dispatch("nextStep", {
+              step: 0
+            })
+          }
+        }
+      });
+    });
+  })
 );
