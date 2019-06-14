ec06814229d4c60dd307768d15da02ad92b838ce
kahverengi <ahmet.duruer@yandex.com>
2019-06-14 17:15:29 +0300

    Changed structure

--- assets/js/router.js
+++ assets/js/router.js
@@ -5,31 +5,31 @@ const router = new VueRouter({
       path: '/',
       name: 'Main',
       component: function (resolve, reject) {
-        loadComponent('Main', '/components/Pages/Main').then(resolve, reject);
+        loadComponent('Main', '/src/pages/Main').then(resolve, reject);
       },
       children: [
         {
           path: '',
           component: function (resolve, reject) {
-            loadComponent('Step_Beginning', '/components/Pages/Main/Steps/Step_Beginning').then(resolve, reject);
+            loadComponent('Step_Beginning', '/src/pages/Main/Steps/Step_Beginning').then(resolve, reject);
           }
         },
         {
           path: 'Step-1',
           component: function (resolve, reject) {
-            loadComponent('Step_1', '/components/Pages/Main/Steps/Step_1').then(resolve, reject);
+            loadComponent('Step_1', '/src/pages/Main/Steps/Step_1').then(resolve, reject);
           }
         },
         {
           path: 'Step-2',
           component: function (resolve, reject) {
-            loadComponent('Step_2', '/components/Pages/Main/Steps/Step_2').then(resolve, reject);
+            loadComponent('Step_2', '/src/pages/Main/Steps/Step_2').then(resolve, reject);
           }
         },
         {
           path: 'Step-3',
           component: function (resolve, reject) {
-            loadComponent('Step_3', '/components/Pages/Main/Steps/Step_3').then(resolve, reject);
+            loadComponent('Step_3', '/src/pages/Main/Steps/Step_3').then(resolve, reject);
           }
         }
       ]
