21b339557d620f1c97602c8799637125a95f1ca2
kahverengi <ahmet.duruer@yandex.com>
2019-06-26 19:46:04 +0300

    Removed unnecessary Main Component from setup UI

--- assets/js/router.js
+++ assets/js/router.js
@@ -3,36 +3,27 @@ const router = new VueRouter({
   routes: [
     {
       path: '/',
-      name: 'Main',
       component: function (resolve, reject) {
-        loadComponent('Main', '/src/pages/Main').then(resolve, reject);
-      },
-      children: [
-        {
-          path: '',
-          component: function (resolve, reject) {
-            loadComponent('Step_Beginning', '/src/pages/Main/Steps/Step_Beginning').then(resolve, reject);
-          }
-        },
-        {
-          path: 'Step-1',
-          component: function (resolve, reject) {
-            loadComponent('Step_1', '/src/pages/Main/Steps/Step_1').then(resolve, reject);
-          }
-        },
-        {
-          path: 'Step-2',
-          component: function (resolve, reject) {
-            loadComponent('Step_2', '/src/pages/Main/Steps/Step_2').then(resolve, reject);
-          }
-        },
-        {
-          path: 'Step-3',
-          component: function (resolve, reject) {
-            loadComponent('Step_3', '/src/pages/Main/Steps/Step_3').then(resolve, reject);
-          }
-        }
-      ]
+        loadComponent('Step_Beginning', '/src/pages/Steps/Step_Beginning').then(resolve, reject);
+      }
+    },
+    {
+      path: 'Step-1',
+      component: function (resolve, reject) {
+        loadComponent('Step_1', '/src/pages/Steps/Step_1').then(resolve, reject);
+      }
+    },
+    {
+      path: 'Step-2',
+      component: function (resolve, reject) {
+        loadComponent('Step_2', '/src/pages/Steps/Step_2').then(resolve, reject);
+      }
+    },
+    {
+      path: 'Step-3',
+      component: function (resolve, reject) {
+        loadComponent('Step_3', '/src/pages/Steps/Step_3').then(resolve, reject);
+      }
     }
   ]
 });
@@ -41,17 +32,17 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   const check = function () {
     if (store.state.stepChecked !== false) {
-      store.state.routePageLoading = true
+      store.state.routePageLoading = true;
 
       next()
     } else {
       setTimeout(check, 100); // check again in a second
     }
-  }
+  };
 
   check()
-})
+});
 
 router.afterEach((to, from) => {
   store.state.routePageLoading = false
-})
+});
