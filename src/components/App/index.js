9b6d4341525c5ba51ce24310ca0dcb36b36d613c
kahverengi <ahmet.duruer@yandex.com>
2019-10-04 10:14:03 +0200

    Fixed dependency loading with queue

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -13,9 +13,6 @@ function loadComponent(componentName, path) {
 }
 
 function LoadCSS(cssURL) {
-
-  // 'cssURL' is the stylesheet's URL, i.e. /css/styles.css
-
   return new Promise(function (resolve, reject) {
 
     const link = document.createElement('link');
@@ -32,85 +29,70 @@ function LoadCSS(cssURL) {
   });
 }
 
-function LoadJS(jsURL) {
-
-  return new Promise(function (resolve, reject) {
-
-    const link = document.createElement('script');
-
-    link.src = jsURL;
-
-    document.head.appendChild(link);
-
-    link.onload = function () {
-      resolve();
-    };
+loadScript("/panel/assets/js/md5.min.js", function () {
+  requirejs.config({
+    paths: {
+      'router': '/assets/js/router',
+      'api-util': '/assets/js/api.util',
+      'storage-util': '/assets/js/storage.util',
+      'vuex-store': '/assets/js/vuex-store',
+      'i18n': '/assets/js/i18n'
+    },
+    shim: {
+      'api-util': ['router'],
+      'storage-util': ['api-util'],
+      'vuex-store': ['storage-util'],
+      'i18n': ['vuex-store']
+    }
   });
-}
 
-requirejs.config({
-  paths: {
-    'router': '/assets/js/router',
-    'api-util': '/assets/js/api.util',
-    'storage-util': '/assets/js/storage.util',
-    'vuex-store': '/assets/js/vuex-store',
-    'i18n': '/assets/js/i18n'
-  },
-  shim: {
-    'api-util': ['router'],
-    'storage-util': ['api-util'],
-    'vuex-store': ['storage-util'],
-    'i18n': ['vuex-store'],
-    'app-dependencies': ['i18n']
-  }
-});
+  requirejs(["i18n"], function () {
+    LoadCSS('/assets/css/style.css').then(function () {
+      ApiUtil.init();
 
-requirejs(["app-dependencies"], function () {
-  LoadCSS('/assets/css/style.css').then(function () {
-    ApiUtil.init();
-
-    const app = new Vue({
-      el: '#app',
-      template: PANO.UI,
-      router,
-      i18n,
-      store,
-      metaInfo() {
-        return {
-          title: this.$t("Common.Page.title"),
-          meta: [
-            {name: 'description', content: this.$t("Common.Page.description")}
-          ]
-        }
-      },
-      methods: {
-        setLang(lang) {
-          this.$store.dispatch('setLang', lang)
-        }
-      },
-      computed: {
-        langLoading() {
-          return this.$store.state.langLoading
+      const app = new Vue({
+        el: '#app',
+        template: PANO.UI,
+        router,
+        i18n,
+        store,
+        metaInfo() {
+          return {
+            title: this.$t("Common.Page.title"),
+            meta: [
+              {name: 'description', content: this.$t("Common.Page.description")}
+            ]
+          }
         },
+        methods: {
+          setLang(lang) {
+            this.$store.dispatch('setLang', lang)
+          }
+        },
+        computed: {
+          langLoading() {
+            return this.$store.state.langLoading
+          },
 
-        routePageLoading() {
-          return this.$store.state.routePageLoading
+          routePageLoading() {
+            return this.$store.state.routePageLoading
+          }
+        },
+        beforeMount() {
+          let loadLanguage;
+
+          if (LanguageUtil.isThereLanguage())
+            loadLanguage = LanguageUtil.getLanguage()
+          else if (navigator.language.toUpperCase() === "tr".toUpperCase() || navigator.language.toUpperCase() === "tr-tr".toUpperCase())
+            loadLanguage = "tr"
+          else
+            loadLanguage = "en"
+
+          loadLanguageAsync(loadLanguage)
+
+          this.$store.dispatch("checkCurrentStep")
         }
-      },
-      beforeMount() {
-        let loadLanguage;
-
-        if (LanguageUtil.isThereLanguage())
-          loadLanguage = LanguageUtil.getLanguage()
-        else if (navigator.language.toUpperCase() === "tr".toUpperCase() || navigator.language.toUpperCase() === "tr-tr".toUpperCase())
-          loadLanguage = "tr"
-        else
-          loadLanguage = "en"
-
-        loadLanguageAsync(loadLanguage)
-
-        this.$store.dispatch("checkCurrentStep")
-      }
+      });
     });
   });
 });
