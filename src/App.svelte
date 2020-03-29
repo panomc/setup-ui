cca20db4759d9c65584b0b09f4e00a417503ba29
kahverengi <ahmet.duruer@yandex.com>
2020-03-29 18:30:38 +0300

    Remove global css & add half of app component from old UI

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,47 @@
-<script>
-  export let name;
-</script>
-
-<main>
-  <h1>Hello {name}!</h1>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style lang="scss">
   @import "commons/scss/main.scss";
 </style>
+
+<div id='MainComponent' class="min-vh-100">
+
+  <div class="container pt-4 mt-4 text-center">
+    <a class="d-inline-block" href="https://panomc.com" target="_blank" title="Pano resmi web sitesini ziyaret edin.">
+      <img alt="Pano Logo" class="d-inline-block m-auto" src="/img/logo-blue.svg" width="24">
+    </a>
+  </div>
+
+  <div class="container d-block m-auto py-3">
+
+    <div class="dropdown text-right" v-show="!langLoading">
+      <a href="javascript:void(0);" aria-expanded="false" aria-haspopup="true"
+         class="nav-link icon-link dropdown-toggle d-inline-block" data-toggle="dropdown" id="selectLanguage"
+         role="button">
+        TR
+      </a>
+      <div aria-labelledby="selectLanguage" class="dropdown-menu dropdown-menu-right">
+        <a class="dropdown-item" href="javascript:void(0);">Türkçe (TR)</a>
+        <a class="dropdown-item" href="javascript:void(0);">English (US)</a>
+      </div>
+    </div>
+
+    <!--  <div class="text-right" v-show="langLoading">-->
+    <!--    <div class="spinner-border spinner-border-sm text-primary" role="status"></div>-->
+    <!--  </div>-->
+
+    <div class="card bg-white shadow setup-bg mb-4 p-0">
+      <div class="col-lg-6 col-10 m-auto py-5">
+
+        <!--      <router-view v-show="!routePageLoading"></router-view>-->
+
+        <div class="text-center">
+          <div class="spinner-border text-primary" role="status"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container py-4 text-center">
+    <small>Pano v.1.0</small>
+  </div>
+
+</div>
