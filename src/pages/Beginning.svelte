ad044c0a607fe90b51103185fcf0baac0a261735
Selim Gökçek <selimgokcek@outlook.com>
2020-04-09 01:25:54 +0300

    Remove unnecessary codes

--- src/pages/Beginning.svelte
+++ src/pages/Beginning.svelte
@@ -3,44 +3,28 @@
   import jQuery from "jquery";
 
   onMount(async () => {
-    jQuery("[data-toggle=\"tooltip\"]").tooltip();
+    jQuery('[data-toggle="tooltip"]').tooltip();
   });
 </script>
 
-<div>
-  <h3 class="pb-3">Pano Kurulumuna Hoş Geldiniz!</h3>
-  <p>
-    Platformu sunucunuza kurduktan sonra kullanmaya başlayabileceksiniz.
-    Pano'nun doğru şekilde çalışabilmesi için aşağıda listelenen bilgiler
-    gerekmekte:
-  </p>
-  <ul>
-    <li>Web sitenizin adı ve açıklaması</li>
-    <li>
-      Veritabanı bilgileri
-      <a
-              class="ml-1"
-              data-placement="right"
-              data-toggle="tooltip"
-              href="javascript:void(0)"
-              title="Bu bilgileri web sunucunuzu sağlayan kurum ile iletişime geçerek
-        öğrenebilirsiniz.">
-        (?)
-      </a>
-    </li>
-    <li>Yönetim paneli için hesap bilgileri</li>
-  </ul>
-  <div class="pt-3 d-block m-auto">
-    <a href="/step-1">
-      <button class="btn btn-primary" type="button">
-        <!--            <span aria-hidden="true"-->
-        <!--                  class="spinner-border spinner-border-sm text-light mx-4"-->
-        <!--                  role="status"></span>-->
-        <!--      <a v-if="!nextButtonLoading">-->
-        <i class="fas fa-arrow-alt-circle-right"/>
-        Başlayalım
-        <!--      </a>-->
-      </button>
+<h3>Pano Kurulumuna Hoş Geldiniz!</h3>
+<p>
+  Platformu sunucunuza kurduktan sonra kullanmaya başlayabileceksiniz. Pano'nun
+  doğru şekilde çalışabilmesi için aşağıda listelenen bilgiler gerekmekte:
+</p>
+<ul>
+  <li>Web sitenizin adı ve açıklaması</li>
+  <li>
+    Veritabanı bilgileri
+    <a
+      data-placement="right"
+      data-toggle="tooltip"
+      href="javascript:void(0)"
+      title="Bu bilgileri web sunucunuzu sağlayan kurum ile iletişime geçerek
+      öğrenebilirsiniz.">
+      (?)
     </a>
-  </div>
-</div>
+  </li>
+  <li>Yönetim paneli için hesap bilgileri</li>
+</ul>
+<a href="/step-1" class="btn btn-primary" role="button">Başlayalım</a>
