2b41796b7ada6238fbebb7d705e5080c4656270c
kahverengi <ahmet.duruer@yandex.com>
2020-04-09 14:03:52 +0300

    Add step 2 page nextStep and backStep logics

--- src/pages/Step-1.svelte
+++ src/pages/Step-1.svelte
@@ -1,3 +1,21 @@
+<script>
+  import { nextStep, backStep, websiteName, websiteDescription } from "../Store";
+
+  function next() {
+    nextStep({
+      step: 1,
+      websiteName: $websiteName,
+      websiteDescription: $websiteDescription
+    });
+  }
+
+  function back() {
+    backStep({
+      step: 1
+    });
+  }
+</script>
+
 <h3>Adım - 1/3</h3>
 <p>
   Websitenizin adını ve açıklamasını belirleyin. Bunlar arama motorlarında
@@ -12,13 +30,15 @@
       id="websiteName"
       class="form-control"
       placeholder="Panocraft"
-      type="text" />
+      type="text"
+      bind:value={$websiteName}
+    />
   </div>
   <div class="form-group">
     <label for="websiteDescription">Açıklama:</label>
-    <textarea id="websiteDescription" class="form-control" rows="2" />
+    <textarea id="websiteDescription" class="form-control" rows="2" bind:value={$websiteDescription}></textarea>
   </div>
 
-  <a href="/step-2" class="btn btn-primary" role="button">Devam Et</a>
-  <a href="/" class="btn btn-outline-primary" role="button">Geri</a>
+  <a href="javascript:void(0);" class="btn btn-primary" role="button" on:click={next}>Devam Et</a>
+  <a href="javascript:void(0);" class="btn btn-outline-primary" role="button" on:click={back}>Geri</a>
 </form>
