95a5f56ca802c59590f4a29cb9f770c6ae43abab
kahverengi <ahmet.duruer@yandex.com>
2020-04-09 12:20:55 +0300

    Add API util

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import jquery from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap';
+import jquery from "jquery";
+import Popper from "popper.js";
+import "bootstrap";
 
-import App from './App.svelte';
+import { ApiUtil } from "./util/api.util";
+import App from "./App.svelte";
+
+ApiUtil.init("http://localhost:8088/api/");
 
 window.jQuery = window.jquery = window.$ = jquery;
 window.Popper = Popper;
@@ -10,6 +13,5 @@ window.Popper = Popper;
 const app = new App({
   target: document.body
 });
-
 
 export default app;
