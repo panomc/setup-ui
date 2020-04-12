3cdcba1c1325a925bb96ca27a5787132a110635d
kahverengi <ahmet.duruer@yandex.com>
2020-04-12 17:04:12 +0300

    Enchant router

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,9 @@
 <script>
-  import router from "page";
-  import { ChunkGenerator } from "svelte-spa-chunk";
-
-  import ChunkComponent from "./Chunk.svelte";
   import { isPageLoading } from "./ChunkStore";
-
   import { stepChecked } from "./Store";
 
   import PageLoading from "./components/PageLoading.svelte";
-
-  const Chunk = ChunkGenerator(ChunkComponent);
-
-  let props = {};
-
-  router("/step-1", () => {
-    props = {
-      component: Chunk(() => import("./pages/Step-1.svelte"))
-    };
-  });
-
-  router("/step-2", () => {
-    props = {
-      component: Chunk(() => import("./pages/Step-2.svelte"))
-    };
-  });
-
-  router("/step-3", () => {
-    props = {
-      component: Chunk(() => import("./pages/Step-3.svelte"))
-    };
-  });
-
-  router("*", () => {
-    props = {
-      component: Chunk(() => import("./pages/Beginning.svelte"))
-    };
-  });
-
-  router.start();
+  import Router from "./components/Router.svelte";
 </script>
 
 <style lang="scss" global>
@@ -84,7 +50,7 @@
         {/if}
 
         {#if $stepChecked}
-          <svelte:component this={props.component} {...props}/>
+          <Router hidden={$isPageLoading}/>
         {/if}
     </div>
   </div>
---
+++ src/components/Router.svelte
@@ -0,0 +1,42 @@
+<script>
+  import router from "page";
+  import { ChunkGenerator } from "svelte-spa-chunk";
+
+  import ChunkComponent from "../Chunk.svelte";
+
+  const Chunk = ChunkGenerator(ChunkComponent);
+
+  let props = {};
+
+  router("/step-1", () => {
+    props = {
+      component: Chunk(() => import("../pages/Step-1.svelte"))
+    };
+  });
+
+  router("/step-2", () => {
+    props = {
+      component: Chunk(() => import("../pages/Step-2.svelte"))
+    };
+  });
+
+  router("/step-3", () => {
+    props = {
+      component: Chunk(() => import("../pages/Step-3.svelte"))
+    };
+  });
+
+  router("*", () => {
+    props = {
+      component: Chunk(() => import("../pages/Beginning.svelte"))
+    };
+  });
+
+  router.start();
+
+  let hidden;
+</script>
+
+<div hidden={hidden}>
+  <svelte:component this={props.component} {...props}/>
+</div>
