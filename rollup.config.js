03f0ac3a54a064f62da628eb3cd7e164b59fb764
kahverengi <ahmet.duruer@yandex.com>
2020-10-21 00:03:55 +0200

    Update dependencies and remove bundle.css

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import autoPreprocess from "svelte-preprocess";
+import sveltePreprocess from "svelte-preprocess";
 import copyTo from "rollup-plugin-copy-assets-to";
 import replace from "@rollup/plugin-replace";
 import livereload from "rollup-plugin-livereload";
@@ -34,13 +34,6 @@ try {
 
 const production = !process.env.ROLLUP_WATCH;
 
-const preprocess = autoPreprocess({
-  scss: {},
-  postcss: {
-    plugins: [require("autoprefixer")],
-  },
-});
-
 const input = ["src/main.js"];
 
 const watch = {
@@ -69,12 +62,8 @@ const plugins = [
   svelte({
     // enable run-time checks when not in production
     dev: !production,
-    // we'll extract any component CSS out into
-    // a separate file - better for performance
-    css: (css) => {
-      css.write("public/assets/css/bundle.css");
-    },
-    preprocess,
+
+    preprocess: sveltePreprocess()
   }),
 
   // If you have external dependencies installed from
