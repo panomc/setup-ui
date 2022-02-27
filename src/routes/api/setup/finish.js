6287c752aa376552b94e1cfb05519cfe6ac15a7e
kahverengi <ahmet.duruer@yandex.com>
2022-02-27 13:27:25 +0300

    Add cookie and add auto login after setup has finished

---
+++ src/routes/api/setup/finish.js
@@ -0,0 +1,36 @@
+import cookie from "cookie";
+
+import * as api from "$lib/api.util.server";
+import generateToken from "$lib/csrf.js";
+import { COOKIE_PREFIX, CSRF_TOKEN_COOKIE_NAME, JWT_COOKIE_NAME } from "$lib/variables.js";
+
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export async function post({ request }) {
+  const body = await request.json();
+
+  const response = await api.post("/setup/finish", JSON.stringify(body));
+
+  const header = {};
+
+  if (response.result === "ok") {
+    const CSRFToken = generateToken();
+
+    header.headers = {
+      "set-cookie": [
+        cookie.serialize(COOKIE_PREFIX + JWT_COOKIE_NAME, response.jwt, {
+          httpOnly: true,
+          path: "/",
+        }),
+        cookie.serialize(COOKIE_PREFIX + CSRF_TOKEN_COOKIE_NAME, CSRFToken, {
+          httpOnly: true,
+          path: "/",
+        }),
+      ],
+    };
+  }
+
+  return {
+    ...header,
+    body: response,
+  };
+}
