45054336affad71f721c8ce7bbe6aa99f3bc6002
kahverengi <ahmet@duruer.dev>
2022-07-31 22:33:07 +0300

    Add form submitting, error alert and default mail service configurations

--- src/lib/pages/Step3.svelte
+++ src/lib/pages/Step3.svelte
@@ -5,102 +5,102 @@
   adresini kullanmasını istiyorsanız onun bilgilerini girin.
 </p>
 
-{#if !chosenService}
-  {#each Object.keys(services) as service, index (service)}
-    <button class="btn" on:click="{() => chooseService(service)}" in:fade
-    >{services[service].name}</button
-    >
-  {/each}
-{:else}
-  <div in:fade>
-    <button class="btn btn-link" on:click="{() => (chosenService = null)}"
-    >← Servisler
-    </button
-    >
-    <div class="mb-3">
-      <div class="form-check form-switch">
-        <input
-          class="form-check-input"
-          type="checkbox"
-          role="switch"
-          id="flexSwitchCheckChecked"
-          bind:checked="{mailConfiguration[chosenService].useSSL}"
-        />
-        <label class="form-check-label" for="flexSwitchCheckChecked"
-        >SSL kullan</label
-        >
-      </div>
-    </div>
+<ErrorAlert error="{error}" />
 
-    <div class="mb-3">
-      <div class="row">
-        <div class="col-6">
-          <div class="mb-3">
-            <label for="sendingAdress">Gönderme Adresi</label>
-            <input
-              class="form-control"
-              id="sendingAdress"
-              type="text"
-              placeholder="no-reply@forexample.com"
-              bind:value="{mailConfiguration[chosenService].address}"
-            />
-          </div>
-        </div>
-        <div class="col-6">
-          <div class="mb-3">
-            <label for="hostAdress">Sağlayıcı Adresi</label>
-            <input
-              class="form-control"
-              id="hostAdress"
-              type="text"
-              placeholder="smtp.forexample.com"
-              bind:value="{mailConfiguration[chosenService].host}"
-            />
-          </div>
-        </div>
-        <div class="w-100"></div>
-        <div class="col-6">
-          <div class="mb-3">
-            <label for="mailUsername">Kullanıcı Adı</label>
-            <input
-              class="form-control"
-              id="mailUsername"
-              type="text"
-              placeholder="no-reply"
-              bind:value="{mailConfiguration[chosenService].username}"
-            />
-          </div>
-        </div>
-        <div class="col-6">
-          <div class="mb-3">
-            <label for="mailUserrPassword">Şifre</label>
-            <input
-              class="form-control"
-              id="mailUserPassword"
-              placeholder="****************"
-              type="password"
-              bind:value="{mailConfiguration[chosenService].password}"
-            />
-          </div>
-        </div>
-        <div class="col-12">
-          <div class="mb-3">
-            <label for="port">Port</label>
-            <input
-              class="form-control"
-              id="post"
-              placeholder="465"
-              type="text"
-              bind:value="{mailConfiguration[chosenService].port}"
-            />
-          </div>
+<form on:submit|preventDefault="{next}">
+  {#if !chosenService}
+    {#each Object.keys(services) as service, index (service)}
+      <button class="btn" on:click="{() => chooseService(service)}" in:fade
+        >{services[service].name}</button
+      >
+    {/each}
+  {:else}
+    <div in:fade>
+      <button class="btn btn-link" on:click="{() => (chosenService = null)}"
+        >← Servisler
+      </button>
+      <div class="mb-3">
+        <div class="form-check form-switch">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            role="switch"
+            id="flexSwitchCheckChecked"
+            bind:checked="{mailConfiguration[chosenService].useSSL}"
+          />
+          <label class="form-check-label" for="flexSwitchCheckChecked"
+            >SSL kullan</label
+          >
         </div>
       </div>
-    </div>
-  </div>
-{/if}
 
-<form on:submit|preventDefault="{submit}">
+      <div class="mb-3">
+        <div class="row">
+          <div class="col-6">
+            <div class="mb-3">
+              <label for="sendingAdress">Gönderme Adresi</label>
+              <input
+                class="form-control"
+                id="sendingAdress"
+                type="text"
+                placeholder="no-reply@forexample.com"
+                bind:value="{mailConfiguration[chosenService].address}"
+              />
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="mb-3">
+              <label for="hostAdress">Sağlayıcı Adresi</label>
+              <input
+                class="form-control"
+                id="hostAdress"
+                type="text"
+                placeholder="smtp.forexample.com"
+                bind:value="{mailConfiguration[chosenService].host}"
+              />
+            </div>
+          </div>
+          <div class="w-100"></div>
+          <div class="col-6">
+            <div class="mb-3">
+              <label for="mailUsername">Kullanıcı Adı</label>
+              <input
+                class="form-control"
+                id="mailUsername"
+                type="text"
+                placeholder="no-reply"
+                bind:value="{mailConfiguration[chosenService].username}"
+              />
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="mb-3">
+              <label for="mailUserrPassword">Şifre</label>
+              <input
+                class="form-control"
+                id="mailUserPassword"
+                placeholder="****************"
+                type="password"
+                bind:value="{mailConfiguration[chosenService].password}"
+              />
+            </div>
+          </div>
+          <div class="col-12">
+            <div class="mb-3">
+              <label for="port">Port</label>
+              <input
+                class="form-control"
+                id="post"
+                placeholder="465"
+                type="text"
+                bind:value="{mailConfiguration[chosenService].port}"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  {/if}
   <div class="row pt-3">
     <div class="col-6">
       <a
@@ -128,56 +128,62 @@
 <script context="module">
   const defaultMailConfiguration = Object.freeze({
     useSSL: true,
-    port: 465
+    port: 465,
   });
 
   export const services = Object.freeze({
     GMAIL: {
       name: "GMail",
       config: {
-        ...defaultMailConfiguration
-      }
+        ...defaultMailConfiguration,
+        host: "smtp.google.com"
+      },
     },
     YAHOO: {
       name: "Yahoo",
       config: {
-        ...defaultMailConfiguration
-      }
+        ...defaultMailConfiguration,
+        host: "smtp.mail.yahoo.com"
+      },
     },
     YANDEX: {
       name: "Yandex",
       config: {
-        ...defaultMailConfiguration
-      }
+        ...defaultMailConfiguration,
+        host: "smtp.yandex.com"
+      },
     },
     MAIL_RU: {
       name: "Mail.ru",
       config: {
-        ...defaultMailConfiguration
-      }
+        ...defaultMailConfiguration,
+        host: "smtp.mail.ru"
+      },
     },
     OUTLOOK: {
       name: "Hotmail / Outlook",
       config: {
-        ...defaultMailConfiguration
-      }
+        ...defaultMailConfiguration,
+        host: "smtp-mail.outlook.com",
+        port: 587
+      },
     },
     OTHER: {
       name: "Diğer",
       config: {
-        ...defaultMailConfiguration
-      }
-    }
+        ...defaultMailConfiguration,
+      },
+    },
   });
 
   /**
    * @type {import("@sveltejs/kit").Load}
    */
   export async function load({
-                               session: {
-                                 stepInfo: { account }
-                               }
-                             }) {
+    session: {
+      stepInfo: { account },
+    },
+  }) {
     return { props: { stepInfo: { account } } };
   }
 </script>
@@ -186,6 +192,8 @@
   import { backStep, nextStep } from "$lib/Store.js";
 
   import { fade } from "svelte/transition";
+  import ApiUtil, { NETWORK_ERROR } from "$lib/api.util.js";
+  import ErrorAlert from "$lib/components/ErrorAlert.svelte";
 
   let loading = false;
   let error = null;
@@ -193,19 +201,14 @@
 
   let mailConfiguration = {};
 
-  $: disabled = !chosenService || chosenService && (!mailConfiguration[chosenService].port || !mailConfiguration[chosenService].address || !mailConfiguration[chosenService].host || !mailConfiguration[chosenService].username || !mailConfiguration[chosenService].password);
-
-  function submit() {
-    error = null;
-
-    if (!loading && !disabled) {
-      loading = true;
-
-      nextStep({
-        step: 3
-      });
-    }
-  }
+  $: disabled =
+    !chosenService ||
+    (chosenService &&
+      (!mailConfiguration[chosenService].port ||
+        !mailConfiguration[chosenService].address ||
+        !mailConfiguration[chosenService].host ||
+        !mailConfiguration[chosenService].username ||
+        !mailConfiguration[chosenService].password));
 
   function back() {
     if (!loading) {
@@ -213,7 +216,7 @@
       error = null;
 
       backStep({
-        step: 3
+        step: 3,
       });
     }
   }
@@ -230,5 +233,32 @@
     }
 
     chosenService = service;
+  }
+
+  function next() {
+    if (disabled) {
+      return;
+    }
+
+    loading = true;
+    error = null;
+
+    ApiUtil.post({
+      path: "/api/setup/mailConfigurationTest",
+      body: mailConfiguration[chosenService],
+    })
+      .then((body) => {
+        if (body.result === "ok") {
+          nextStep({
+            step: 3,
+            ...mailConfiguration[chosenService],
+          });
+        } else if (body.error) {
+          showError(body.error);
+        } else showError(NETWORK_ERROR);
+      })
+      .catch(() => {
+        showError(NETWORK_ERROR);
+      });
   }
 </script>
