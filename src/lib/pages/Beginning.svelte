cd604044bbaeee1986f85dbdb3b482775caa91bd
Selim Gökçek <selimgokcek@outlook.com>
2023-10-03 19:32:52 +0300

    Add language selection to Beginning page

--- src/lib/pages/Beginning.svelte
+++ src/lib/pages/Beginning.svelte
@@ -1,19 +1,54 @@
 <div class:opacity-75="{disabled}">
-  <h3>Hoş geldiniz</h3>
+  <h4>Hoş geldiniz</h4>
   <p class="text-muted">
     Yukarıda listelenen adımlarla ilgili gereksinimleri belirleyerek Pano'yu ağ
     sunucunuza kurabilirsiniz.
   </p>
 </div>
 
+<h5>Dil seçin</h5>
+
+<ul class="list-group">
+  <li class="list-group-item">
+    <input
+      class="form-check-input me-1"
+      type="radio"
+      name="langSelectionRadio"
+      value=""
+      id="langENRadio"
+      aria-checked="true"
+      checked />
+    <label class="form-check-label" for="langENRadio">ingilizce (US)</label>
+  </li>
+  <li class="list-group-item">
+    <input
+      class="form-check-input me-1"
+      type="radio"
+      name="langSelectionRadio"
+      value=""
+      id="langTRRadio" />
+    <label class="form-check-label" for="langTRRadio">Türkçe (TR)</label>
+  </li>
+</ul>
+
 <div class="row pt-3">
-  <button
-    class="btn btn-primary"
-    on:click="{start}"
-    class:disabled="{loading || disabled}"
-    disabled="{loading || disabled}">
-    İleri
-  </button>
+  <div class="col-6">
+    <button
+      type="button"
+      aria-disabled="true"
+      class="btn btn-lg btn-link w-100 disabled">
+      Geri
+    </button>
+  </div>
+  <div class="col-6">
+    <button
+      class="btn btn-lg btn-primary w-100"
+      on:click="{start}"
+      class:disabled="{loading || disabled}"
+      disabled="{loading || disabled}">
+      İleri
+    </button>
+  </div>
 </div>
 
 <script>
